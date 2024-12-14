4ad9b2401502fd95f7f7edf369411bcb363f0c60
genevera <genevera@noreply.users.github.com>
2024-12-14 00:51:24 -0500

    ok fuck that

--- demo/realtime-img2img/img2img.py
+++ demo/realtime-img2img/img2img.py
@@ -18,7 +18,7 @@ from pydantic import BaseModel, Field
 from PIL import Image
 import math
 
-base_model = "stabilityai/sdxl-turbo"
+base_model = "stabilityai/sd-turbo"
 taesd_model = "madebyollin/taesd"
 
 default_prompt = """Batman villainess "Poison Ivy", detailed professional photograph, intricate details, amazing color, crisp cinematic lighting, sharp focus, award winning photograph"""
@@ -64,11 +64,11 @@ class Pipeline:
         #     id="negative_prompt",
         # )
         width: int = Field(
-            1024, min=2, max=15, title="Width", disabled=True, hide=True, id="width"
+            512, min=2, max=15, title="Width", disabled=True, hide=True, id="width"
 
         )
         height: int = Field(
-            1024, min=2, max=15, title="Height", disabled=True, hide=True, id="height"
+            512, min=2, max=15, title="Height", disabled=True, hide=True, id="height"
         )
 
     def __init__(self, args: Args, device: torch.device, torch_dtype: torch.dtype):
